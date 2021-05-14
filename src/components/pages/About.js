473e58bff59709a2f2bf8e812924af78b20fa6b7
memeTorres1511 <42261231+memeTorres1511@users.noreply.github.com>
2021-05-13 23:18:39 -0600

    Cambio nombre
    cambio en nombre al about us

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -29,7 +29,7 @@ export default function About() {
               <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRZ9DMNFHxwZcfPXJrJeBMITxPMP3FMZk_ixXzTfzt4G_C-G058"></img>
             </figure>
             <div class="contenido-card">
-		          <h3> Erick Manuel Aguilar Siguache </h3>
+		          <h3> Manuel Alejandro Garcia Torres </h3>
 		          <p> Estudiante de ingenieria en sistemas informaticos </p>
 	          </div>
           </div>
