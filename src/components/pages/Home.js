8cdca3248c98db57c8f70c125ef76a2fe328c949
Ernesto2909 <64288541+Ernesto2909@users.noreply.github.com>
2021-05-15 07:28:23 -0600

    Cambios visuales al home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../App.css';
+import logo from '../../images/logo.png';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Home() {
@@ -7,29 +8,14 @@ export default function Home() {
     <>
       <section className="bodyClass">
         <article class="textos-main">
-                <img src="./src/images/logo.png"></img>
+          <img src={logo} width="170"/>
                 <h2></h2>
                 <h2></h2>
-                <h2> Libros de todo los tipos </h2>
+                <h1> Libros de todo los tipos </h1>
+                <h2> Para tu educacion y entretenimiento </h2>
         </article>
       </section>
       <div class="site-wrap"></div>
-
-        
-      <h1 className="title-cards">Inicio</h1>
-      <div className="container">
-      <div className="mb-3">
-      <label for="exampleFormControlInput1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
-      </div>
-<div className="mb-3">
-  <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
-      </div>
-
-      
-      
     </>
   );
 }
